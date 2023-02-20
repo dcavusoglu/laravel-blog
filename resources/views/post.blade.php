70a49f8f255d43b5cdfeeddd605a1eaf5f4b2a1f
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2023-02-20 17:08:20 +0300

    init commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Post</title>
+  <article>
+    <?= $post; ?>
+  </article>
+  <a href="/">Go back</a>
+</head>
+<body>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $files = File::files(resource_path("posts"));
+    $posts = [];
+    foreach ($files as $file) {
+        $document = YamlFrontMatter::parseFile($file);
+
+        $posts[] = new Post (
+            $document->title,
+            $document->slug,
+            $document->excerpt,
+            $document->date,
+            $document->body()
+        );
+    }
+
+    // ddd($document->slug);
+
+    return view('posts', [
+      'posts' => $posts
+    ]);
+});
+
+
+
+
+Route::get('posts/{post}', function ($slug) {
+
+  return view('post', [
+    'post' => Post::find($slug)
+  ]);
+})->where('post', '[A-z_\-]+');
