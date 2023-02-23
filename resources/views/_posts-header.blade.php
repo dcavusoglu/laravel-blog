0f5ea287edb7e2cc23849342c34ae2b51ba35f2a
Duygu Cavusoglu <cvsgl.dyg@gmail.com>
2023-02-23 14:57:38 +0300

    dropdown items separated

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -17,8 +17,7 @@
             <x-dropdown>
 
                 <x-slot name="trigger">
-                    <button @click="show = !show"
-                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
                         {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
                         <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;"
                             width="22" height="22" viewBox="0 0 22 22">
@@ -35,8 +34,7 @@
                 <x-dropdown-item href="/">All</x-dropdown-item>
 
                 @foreach ($categories as $category)
-                    <x-dropdown-item href="/categories/{{ $category->slug }}"
-                        class="{{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }} ">
+                    <x-dropdown-item href="/categories/{{ $category->slug }}" :active="request()->is('categories/' . $category->slug)">
                         {{ ucwords($category->name) }}</x-dropdown-item>
                 @endforeach
             </x-dropdown>
--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,6 +1,10 @@
 @props(['trigger'])
+
+{{-- this component available because it should not be leaking out into our view --}}
 <div x-data="{ show: false }" @click.away="show = false">
-    {{ $trigger }}
+    <div @click="show = !show">
+        {{ $trigger }}
+    </div>
     <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display:none">
         {{ $slot }}
     </div>
